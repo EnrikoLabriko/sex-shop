3a90c59a33ee8c6adde254ef2e1aff633c1ad373
Alexander Telyatnikov <labriko@yandex.ru>
2017-07-03 00:43:00 +0300

    feat(storybook): Add custom storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,35 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add addional webpack configurations.
-// For more information refer the docs: https://getstorybook.io/docs/configurations/custom-webpack-config
+const path = require('path');
 
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
+const projectRootPath = path.resolve(__dirname, '..');
 
 module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
   module: {
-    loaders: [
-      // add your custom loaders.
-    ],
-  },
+    rules: [
+      {
+        test: /\.styl$/,
+        include: path.resolve(projectRootPath, 'src'),
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          {
+            loader: 'stylus-loader',
+          }
+        ],
+      }
+    ]
+  }
 };
